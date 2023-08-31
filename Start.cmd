3e53d020b5fc1b3e02c83b385aeca4175aacf734
heresfilly <78676346+heresfilly@users.noreply.github.com>
2023-08-31 15:36:38 -0400

    Mediafire Files Upload
    Couldn't add files folder directly into GitHub, so I uploaded it to Mediafire (LINK IN INSTALLATION.TXT)

---
+++ Start.cmd
@@ -0,0 +1,3 @@
+mkdir save
+cd .\save
+..\files\azul-1.8.9_345\bin\java.exe -noverify -Xms4096m -Xmx4g -Djava.library.path=..\files\1.8.9-natives-win -cp "..\files\RiseCompressed.jar;lwjgl.jar;lwjgl_util.jar" net.minecraft.client.main.Main -uuid fc5bc365-aedf-30a8-8b89-04e462e29bde -accessToken yes -version 1
